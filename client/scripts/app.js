a9db2c4544d5dd83a5b603df6d7ac42593b553d9
Hack Reactor Student <student@hackreactor.com>
2017-08-25 16:36:19 -0500

    rendering messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,20 +9,53 @@
 var app = {
   
   
-  server: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',  
-  init: function() {},
+  server: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages', 
+   
+  init: function() {
+    
+  },
   send: function(val) {
     $.ajax({
       type: 'POST',
-      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',  
-      data: val
+      url: this.server,  
+      data: JSON.stringify(val),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+
     });
   },
   fetch: function() {
     $.ajax({
       type: 'GET',
-      url: this.server
+      url: this.server,
+      success: function(messages) {
+        //console.log(messages);
+        messages.results.forEach(function(obj) {
+          console.log(obj);
+          app.renderMessage(obj);
+        });
+      }
     });
+  },
+  
+  clearMessages: function () {
+    $('#chats').empty();
+  },
+  
+  renderMessage: function (obj) {  
+    console.log(obj);
+    $('#chats').append('<ul class="chat">@' + obj.username + ': ' + obj.text + '</ul>');
+    //double check to see if supposed to be <p>
+  },
+  
+  renderRoom: function (roomName) {
+    $('#roomSelect').append('<div id="' + roomName + '"> </div>');
   }
 
 };
